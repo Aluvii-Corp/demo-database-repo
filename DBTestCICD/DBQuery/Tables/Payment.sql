350d133cac5352705c9bb518da1a5ebf7e768f95
Somendra Kumar Tiwari <somendratiwari@hotmail.com>
2025-05-19 11:46:05 +0530

    Update folder structure

---
+++ DBTestCICD/DBQuery/Tables/Payment.sql
@@ -0,0 +1,17 @@
+﻿CREATE TABLE [Transaction.Payment]
+(
+	[PaymentId] [int] IDENTITY(1,1) NOT NULL,
+	[TransactionId] [int] NOT NULL,
+	[PaymentTypesId] [int] NOT NULL,
+	[PaymentAmount] [decimal](18, 2) NULL,
+	[CreditCardAuthorizationNumber] [varchar](20) NULL,
+	[CustomerName] [varchar](100) NULL,
+	[CustomerIpAddress] [varchar](50) NULL,
+	[GiftCardId] [int] NULL,
+	[CashTendered] [decimal](18, 2) NULL,
+ CONSTRAINT [PK_Transaction.Payment] PRIMARY KEY CLUSTERED 
+(
+	[PaymentId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, FILLFACTOR = 80, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
---
+++ DBTestCICD/DBQuery/Tables/Transactions.sql
@@ -0,0 +1,19 @@
+﻿CREATE TABLE [Transaction.Transactions]
+(
+	[TransactionId] [int] IDENTITY(1,1) NOT NULL,
+	[CreatedOn] [datetime] NOT NULL,
+	[CreatedBy] [int] NOT NULL,
+	[UpdatedOn] [datetime] NOT NULL,
+	[UpdatedBy] [int] NOT NULL,
+	[IsDeleted] [bit] NOT NULL,
+	[IsActive] [bit] NOT NULL,
+	[LocationId] [int] NOT NULL,
+	[DepartmentId] [int] NOT NULL,
+	[SiteId] [int] NOT NULL,
+	[RegisterId] [int] NOT NULL,
+CONSTRAINT [PK_dbo.[Transaction.Transactions]]] PRIMARY KEY CLUSTERED 
+(
+	[TransactionId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, FILLFACTOR = 80, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
